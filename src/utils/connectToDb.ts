dd6f0f34fa56613ad556f541e9ebccb12b3f0667
Olek Golus <olekgolus02@gmail.com>
2024-03-14 00:09:04 +0100

    Docker fixes

--- src/utils/connectToDb.ts
+++ src/utils/connectToDb.ts
@@ -1,14 +1,9 @@
 import mongoose from "mongoose";
-import { dbUrl, dbUser, dbPassword } from "./environments";
+import { dbUrl } from "./environments";
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect(dbUrl, {
-            auth: {
-                username: dbUser,
-                password: dbPassword,
-            },
-        });
+        await mongoose.connect(dbUrl);
         console.info("🗄️  Connected to database");
     } catch (error: any) {
         console.error("Error connecting to database", error);
--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -2,5 +2,3 @@ export const appPort = process.env.APP_PORT || 8080;
 export const dbType = process.env.DB_TYPE || "mongodb";
 
 export const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/";
-export const dbUser = process.env.DB_USER || "admin";
-export const dbPassword = process.env.DB_PASSWORD || "password";
